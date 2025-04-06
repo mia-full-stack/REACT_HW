8232dab26f90b924a13d5f564d028808f28075c1
MIA <mia.full.stack@gmail.com>
2025-04-06 18:35:13 +0200

    Новое сообщение

---
+++ HW_10_загрузка катрочек с Api/src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import CatImage from "../CatImage/CatImage"
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <main className="container">
+        <h1 className="title">Случайные картинки кошек</h1>
+        <CatImage />
+      </main>
+    </>
+  )
+}
+
+export default App
